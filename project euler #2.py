ccac104a319afa609ea65f61bbee7e0f3813682e
Fred Herbert <busterkeiton@gmail.com>
2019-06-08 12:48:50 +0200

    Update project euler #2.py

--- project euler #2.py
+++ project euler #2.py
@@ -14,6 +14,12 @@ def fibo(x,z):
                 break
             Fibo_even.append(y)
 
+    for x in range(x,100):
+            y = int(((((1+math.sqrt(5))/2)**x)-(((1-math.sqrt(5))/2)**x))/math.sqrt(5))
+            if y % 2 == 0:
+                if y > z:
+                    break
+                Fibo_even.append(y)
 fibo(5,4000000)
 print(sum(Fibo_even))
 end = time.time()
