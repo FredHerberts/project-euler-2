a46d7a356a34c43fc1d9f42f7f367caf104bf6fb
FredHerberts <51488709+FredHerberts@users.noreply.github.com>
2019-06-08 12:51:27 +0200

    Update project euler #2.py

--- project euler #2.py
+++ project euler #2.py
@@ -2,18 +2,7 @@ import math
 import time
 Fibo_even = [2]
 start = time.time()
-
 def fibo(x,z):
-    loop = True
-    while loop == True:
-        y = int(((((1+math.sqrt(5))/2)**x)-(((1-math.sqrt(5))/2)**x))/math.sqrt(5))
-        x += 1
-        if y % 2 == 0:
-            if y > z:
-                loop = False
-                break
-            Fibo_even.append(y)
-
     for x in range(x,100):
             y = int(((((1+math.sqrt(5))/2)**x)-(((1-math.sqrt(5))/2)**x))/math.sqrt(5))
             if y % 2 == 0:
