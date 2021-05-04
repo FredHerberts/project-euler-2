aaf0cf5638a9ccbf5a8709c26acd545d79e6ca52
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2021-05-04 18:06:27 +0200

    Update project euler #2.py

--- project euler #2.py
+++ project euler #2.py
@@ -3,13 +3,12 @@ import time
 Fibo_even = [2]
 start = time.time()
 def fibo(x,z):
-    for x in range(x,100):
+    for x in range(x,100,3):
             y = int(((((1+math.sqrt(5))/2)**x)-(((1-math.sqrt(5))/2)**x))/math.sqrt(5))
-            if y % 2 == 0:
-                if y > z:
-                    break
-                Fibo_even.append(y)
-fibo(5,4000000)
+            if y > z:
+                break
+            Fibo_even.append(y)
+fibo(6,4000000)
 print(sum(Fibo_even))
 end = time.time()
 print(end - start)
